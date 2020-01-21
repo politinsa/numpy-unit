c64d845e6635d1014e91d9a9654aefe933388279
toto <toto@toto.com>
2020-01-21 19:30:47 +0100

    First

---
+++ docs/index.rst
@@ -0,0 +1,28 @@
+.. numpy_unit documentation master file, created by
+   sphinx-quickstart on Tue Jan 21 17:43:15 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. include:: ../README.rst
+
+class Unit
+==========
+.. autoclass:: numpy_unit.Unit
+    :members:
+
+class ArrayUnit
+===============
+.. autoclass:: numpy_unit.ArrayUnit
+    :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+print(setuptools.find_packages())
+setuptools.setup(
+    name="numpy_unit",
+    version="0.1.0",
+    author="Politinsa",
+    description="A package providing an unit system for numpy multidimensionnal arrays.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/politinsa/numpy-unit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["numpy"]
+)
